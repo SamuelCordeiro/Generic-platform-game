72d103c5b13cf61dffb81ff7c2c0899547070a52
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-11-05 19:25:35 -0300

    Saw script modified.

---
+++ Assets/Scripts/Pitfalls/Saw.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saw : MonoBehaviour
+{
+    // public float speed;
+    // public float moveTime;
+    // private bool directionRight;
+    // private float timer;
+    [SerializeField] private float speed;
+    [SerializeField] private List<Transform> points;
+    [SerializeField] private int pointsCount;
+    // Start is called before the first frame update
+    void Start(){}
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if(directionRight)
+        // {
+        //     transform.Translate(Vector2.right * speed * Time.deltaTime);
+        //     transform.eulerAngles = new Vector3(0f,0f,0f);
+        // }
+        // else
+        // {
+        //     //transform.Translate(Vector2.left * speed * Time.deltaTime);
+        //     transform.eulerAngles = new Vector3(0f,180f,0f);
+        // }
+        // transform.Translate(Vector2.right * speed * Time.deltaTime);
+        // timer += Time.deltaTime;
+        // if(timer >= moveTime)
+        // {
+        //     directionRight = !directionRight;
+        //     timer = 0f;
+        // }
+        Move();
+    }
+
+    void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, points[pointsCount].position, speed * Time.deltaTime);
+        
+        if(transform.position == points[pointsCount].transform.position)
+        {
+            pointsCount += 1;  
+            transform.eulerAngles = new Vector3(0f,0f,0f);
+        }
+        if(pointsCount == points.Count)
+        {
+            pointsCount = 0;
+            transform.eulerAngles = new Vector3(0f,180f,0f);
+        }
+    }
+}
