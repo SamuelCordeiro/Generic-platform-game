2fccd848a8d06d95f55c30531c998b4844b0c42a
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-12-12 03:24:50 -0300

    Rino script created.

---
+++ Assets/Scripts/Enemies/Rino.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rino : MonoBehaviour
+{
+    [SerializeField] private int life;
+    [SerializeField] private float speed;
+    [SerializeField] private Transform head;
+    private Rigidbody2D rig;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
