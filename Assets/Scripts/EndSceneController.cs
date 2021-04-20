7db7b6316911b9060b50ed3a81a92d99ab4bddf5
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2021-04-20 04:02:18 -0300

    End Scene controller created.

---
+++ Assets/Scripts/EndSceneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndSceneController : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> test = new List<GameObject>();
+    [SerializeField] private GameObject nextLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < test.Count; i++)
+        {
+            if(test[i] == null)
+            {
+                test.RemoveAt(i);
+            }
+        }
+
+        if(test.Count == 0)
+        {
+            nextLevel.SetActive(true);
+        }
+    }
+}
