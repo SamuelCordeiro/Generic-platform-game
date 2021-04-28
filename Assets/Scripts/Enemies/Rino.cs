2a25e097477d4aa6e491142d55162dae8884549b
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2021-04-28 19:54:27 -0300

    Rino script modified.

--- Assets/Scripts/Enemies/Rino.cs
+++ Assets/Scripts/Enemies/Rino.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class Rino : MonoBehaviour
 {
-    [SerializeField] private int life;
+    private float initialSpeed;
     [SerializeField] private float speed;
+    [SerializeField] private int life;
+    [SerializeField] private float hitTime;
     [SerializeField] private Transform head;
     [SerializeField] private List<Transform> points;
     [SerializeField] private int pointsCount;
+    private bool canWalk;
     private Rigidbody2D rig;
     private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
+        canWalk = true;
+        initialSpeed = speed;
         rig = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
@@ -26,17 +31,22 @@ public class Rino : MonoBehaviour
 
     void Move()
     {
-        transform.position = Vector2.MoveTowards(transform.position, points[pointsCount].position, speed * Time.deltaTime);
-
-        if (transform.position == points[pointsCount].transform.position)
+        if (canWalk)
         {
-            pointsCount += 1;
-            transform.eulerAngles = new Vector3(0f, 180f, 0f);
-        }
-        if (pointsCount == points.Count)
-        {
-            pointsCount = 0;
-            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            transform.position = Vector2.MoveTowards(transform.position, points[pointsCount].position, speed * Time.deltaTime);
+            anim.SetBool("run", true);
+            if (transform.position == points[pointsCount].transform.position)
+            {
+                pointsCount += 1;
+                anim.SetTrigger("hitWall");
+                StartCoroutine(hitWall());
+                transform.eulerAngles = new Vector3(0f, 180f, 0f);
+            }
+            if (pointsCount == points.Count)
+            {
+                pointsCount = 0;
+                transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            }
         }
     }
 
@@ -47,10 +57,14 @@ public class Rino : MonoBehaviour
             float height = collision.contacts[0].point.y - head.position.y;
             if (height > 0)
             {
+                life--;
+                anim.SetTrigger("hit");
                 collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 12, ForceMode2D.Impulse);
-                speed = 0;
-                anim.SetTrigger("die");
-                Destroy(gameObject, 0.35f);
+                if (life <= 0)
+                {
+                    speed = 0;
+                    Destroy(gameObject, 0.35f);
+                }
             }
             else
             {
@@ -58,5 +72,16 @@ public class Rino : MonoBehaviour
                 Destroy(collision.gameObject);
             }
         }
+    }
+
+    IEnumerator hitWall()
+    {
+        anim.SetBool("run", false);
+        speed = 0;
+        canWalk = false;
+        yield return new WaitForSeconds(hitTime);
+        anim.SetBool("run", true);
+        speed = initialSpeed;
+        canWalk = true;
     }
 }
