1966c62a403f3285a97590a9c0688f01a14054eb
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2021-05-13 01:02:11 -0300

    Trampoline platform script organized.

---
+++ Assets/Scripts/Platforms/Trampoline.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    public float jumpForce;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            anim.SetTrigger("Jump");
+            //collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpForce;
+        }
+    }
+}
